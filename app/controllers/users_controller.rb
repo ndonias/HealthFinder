1f236dff19a1faa4ae22dacf8699b2daf6b07603
Jane Alexander <jalexanderny@earthlink.net>
2015-03-03 12:36:07 -0500

    Generated Users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  
+
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/create'
+
+  get 'users/delete'
+
+  resources :users
+
   get 'home/index'
 
   root 'home#index'
