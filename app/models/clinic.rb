f2dd5700a544d64ba65d27becbe61b0ddbbaeb16
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-03-28 14:00:45 -0400

    converted to csv

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,5 +1,7 @@
 class Clinic
 
+	require 'csv'
+
 	attr_accessor :name, :address, :latitude, :longitude, :zip, :borough, :number
 
 	def initialize(name)
@@ -7,52 +9,12 @@ class Clinic
 		@zip = zip
 		@borough = borough
 		@number = number
-		# @latitude = latitude
-		# @longitude = longitude
 	end
 
-	# def self.all
-	# 	CSV.foreach('clinics.csv') do |clinic|
-	# 	  c = Clinic.new(clinic[0])
-	# 	  if clinic[1].present?
-	# 	  	c.address = clinic[1]
-	# 	    c.latitude = clinic[7]
-	# 		c.longitude = clinic[8]
-	# 	  else
-	# 	  	c.address = "Unknown"
-	# 	  end
-	# 	  c
-	# 	end
-	# end
-
 	def Clinic.all
-		# Authorizes with OAuth and gets an access token.
-		client = Google::APIClient.new
-		auth = client.authorization
-		auth.client_id = ENV["CLINIC_CLIENT_ID"]
-		auth.client_secret = ENV["CLINIC_CLIENT_SECRET"]
-		auth.scope =
-		    "https://www.googleapis.com/auth/drive " +
-		    "https://spreadsheets.google.com/feeds/"
-		# auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
-		# print("1. Open this page:\n%s\n\n" % auth.authorization_uri)
-		# print("2. Enter the authorization code shown in the page: ")
-		# auth.code = $stdin.gets.chomp
-		# auth.fetch_access_token!
-		# puts "*" * 40
-		# puts auth.access_token
-		# puts "*" * 40
-		# ENV["CLINIC_ACCESS_TOKEN"]
-		access_token = "ya29.LgEvT_JIWUx5liUzmZ_WBWlhqYbCew3HO-DC8Q8OPDF77mt4fXOcGWuMLSf6C0zgOHg4HqRjf4uJOw"		
-		# ENV["CLINIC_ACCESS_TOKEN"]
-		# Creates a session.
-		session = GoogleDrive.login_with_oauth(access_token)
-
-		# First worksheet of
-		# https://docs.google.com/spreadsheets/d/1SSW62yek7W45phsgcaZwBdo4Buyx9-DvLQRCmxGJYYE
-		ws = session.spreadsheet_by_key("1SSW62yek7W45phsgcaZwBdo4Buyx9-DvLQRCmxGJYYE").worksheets[0]
-		ws.rows
-		result = ws.rows.map do |clinic|
+		csv_text = File.read('clinics.csv')
+		csv = CSV.parse(csv_text, headers: false)
+		result = csv.map do |clinic|
 		  c = Clinic.new(clinic[0])
 		  if clinic[1].present?
 		  	c.address = clinic[1]
