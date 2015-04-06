785fb74b655cf6e85cf730fd80c2c89604a5e32e
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-06 17:53:48 -0400

    styled map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,14 @@ class HomeController < ApplicationController
 	  	# end
 		@hash = @clinics.map do |clinic|
 			html = "<h4>#{clinic.name}</h4>Phone: #{clinic.number}</br>Address: #{clinic.address}"
-			{lat: clinic.latitude.to_s, lng: clinic.longitude.to_s, description: clinic.name, title: clinic.name, borough: clinic.borough, infowindow: html, sidebar: html}
+			{lat: clinic.latitude.to_s, lng: clinic.longitude.to_s, 
+			description: clinic.name, title: clinic.name, borough: clinic.borough,
+			picture: {
+	        	url: "http://icons.iconarchive.com/icons/medicalwp/medical/24/Hospital-blue-icon.png",
+	        	width:  25,
+	        	height: 25
+      		}, 
+			infowindow: html, sidebar: html}
      	end
      end
 end
