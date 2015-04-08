8211637d80f625454ad0bd1047ac9bd1652a478a
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-08 14:46:40 -0400

    removed comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :comments
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  	has_many :comments, dependent: :destroy
   	has_many :articles
 
   	def admin?
