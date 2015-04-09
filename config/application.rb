5b7532b4da63992a46350dbdbe5e4b7673a0a6bf
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-09 18:00:02 -0400

    assets updated

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
-
 
 module HealthFinder
   class Application < Rails::Application
@@ -21,7 +19,6 @@ module HealthFinder
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
 
   end
 end
