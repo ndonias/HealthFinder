f09540bf0ee55b5b2f9f2607edd5979fd0cb8cf8
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-09 18:07:04 -0400

    assets added

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+config.serve_static_assets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,7 +20,6 @@ module HealthFinder
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
 
   end
 end
